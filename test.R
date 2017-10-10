fb057673e180c98cf05737931f9afa868ba0da7d
Brittany Baker <b.baker.567@gmail.com>
2017-10-10 13:32:52 -0700

    Yet another test

--- test.R
+++ test.R
@@ -2,10 +2,11 @@
 library(seqinr)
 
 
-#Hello, this is Brittany
-#Test is not working
+#Test 2
 =======
 #load seqinr package
 library(seqinr)
 
 >>>>>>> 441b8731a691d38ae180dc111f6a275d89b0dd1c
+
+#Test 3
