10669d7ed129b743703f2a26c17c6dc89f9316cc
dlaointhebay <32553991+dlaointhebay@users.noreply.github.com>
2017-10-17 10:54:41 -0700

    Add files via upload

---
+++ derek's attempt.R
@@ -0,0 +1,26 @@
+read.csv('HIV.csv') -> D
+
+D$WTnt->E
+#looks at only the Wild type nucleotide column and stores into E
+
+data.frame("Wildtype"=E,"Possible"=TRUE) ->data
+Wildtype<-length(E)
+
+HIV_possible_cPg <- function(Wildtype) {
+  
+  if {Wildtype[x]=="c" & Wildtype[x+1]=="g",print="yes"}
+
+ else print="no"
+  
+  if {Wildtype[x]=="c" & Wildtype[x+1]=="a", print="yes"}
+ 
+else print="no"
+  
+  if {Wildtype[x]=="c" & Wildtype[x+1]=="a", print="yes"}
+
+return(print="no")
+}
+
+function(3)
+# not complete 
+  
